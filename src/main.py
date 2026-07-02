aef39d602df63cb906c51b4512ce3099daffac7f
jrcaro <caroromerojuan@gmail.com>
2026-07-02 22:36:27 +0200

    First commit

---
+++ src/main.py
@@ -0,0 +1,64 @@
+import datetime
+import os.path
+import argparse
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+def authenticate_google():
+    credentials, project_id = load_credentials_from_file(
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
+        scopes=SCOPES
+    )
+
+    return build("calendar", "v3", credentials=creds)
+
+def main(calendar_id):
+  try:
+    service = authenticate_google
+
+    # Call the Calendar API
+    now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+    print("Getting the upcoming 10 events")
+    events_result = (
+        service.events()
+        .list(
+            calendarId=calendar_id,
+            timeMin=now,
+            maxResults=10,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events = events_result.get("items", [])
+
+    if not events:
+      print("No upcoming events found.")
+      return
+
+    # Prints the start and name of the next 10 events
+    for event in events:
+      start = event["start"].get("dateTime", event["start"].get("date"))
+      print(start, event["summary"])
+
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="CLI tool to copy Decathlon planning to Google calendar"
+    )
+    parser.add_argument(
+        "--calendar-id", required=True, type=str, help="Google calendar ID"
+    )
+    
+    args = parser.parse_args()
+    input = {key: value for key, value in args.__dict__.items() if value is not None}
+    main(**input)
